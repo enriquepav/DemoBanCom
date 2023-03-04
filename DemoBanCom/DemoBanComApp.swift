a1a0d95da8064aa2ff97e883a3140d9eba8725a4
Enrique Alata <enrique.alata_nttdata@scotiabank.com.pe>
2023-03-04 08:32:45 -0500

    Initial Commit

---
+++ DemoBanCom/DemoBanComApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoBanComApp.swift
+//  DemoBanCom
+//
+//  Created by MAC1DIGITAL20 on 4/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoBanComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
